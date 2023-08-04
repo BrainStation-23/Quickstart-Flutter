7cd860e7d228a6ea70197dcbd53e691119f0792e
Md. Shahin Bashar <sbiitju@gmail.com>
2023-08-04 15:24:33 +0600

    refactoring code, routing configuration done,common app text field added

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -3,11 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 
 import 'core/bloc/bloc_observer.dart';
-import 'core/bloc/global_bloc_providers.dart';
 import 'core/di/injection_container.dart' as di;
-import 'core/localization.dart';
-import 'core/theme/color.schema.dart';
-import 'core/utils/transitions.dart';
 import 'flavors/build_config.dart';
 import 'flavors/env_config.dart';
 import 'flavors/environment.dart';

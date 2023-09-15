654c5598eb7c645eb6fa960b0835c608e88b995e
Md. Shahin Bashar <sbiitju@gmail.com>
2023-09-15 17:23:55 +0600

    selector added in signup

--- lib/features/sign_up/presentation/pages/sign_up_screen.dart
+++ lib/features/sign_up/presentation/pages/sign_up_screen.dart
@@ -5,13 +5,13 @@ import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:qs_flutter/core/base/widgets/app_spacer.dart';
 import 'package:qs_flutter/core/utils/utils.dart';
+import 'package:qs_flutter/features/sign_up/presentation/bloc/sign_up_selector.dart';
 
 import '../../../../core/base/widgets/app_primary_button.dart';
 import '../../../../core/base/widgets/app_textfield.dart';
 import '../../../../core/routes/routes.dart';
 import '../../../../core/validators/input_validators.dart';
 import '../../../../core/values/app_assets_path.dart';
-import '../../../../core/values/app_colors.dart';
 import '../../../../core/values/app_values.dart';
 import '../bloc/sign_up_bloc.dart';
 
@@ -36,27 +36,28 @@ class _SignUpScreenState extends State<SignUpScreen> {
   Widget build(BuildContext context) {
     _appLocalizations = AppLocalizations.of(context);
     return Scaffold(
-      body: BlocConsumer<SignUpBloc, SignUpState>(
-        listener: (context, state) {
-          if (state.status == SignUpStatus.success) {
-            _firstNameController.text = '';
-            _lastNameController.text = '';
-            _passwordController.text = '';
-            _birthDateController.text = '';
-            _emailController.text = '';
-            _phoneNumberController.text = '';
-            context.read<SignUpBloc>().add(SignUpStatusChange());
-          } else if (state.status == SignUpStatus.failure) {
-            showSnackBarMessage(context, _appLocalizations?.failedMessage ?? "",
-                SnackBarMessageType.failure);
-          } else if (state.status == SignUpStatus.verify_otp) {
-            showSnackBarMessage(
-                context, state.responseMessage, SnackBarMessageType.success);
-            context.goNamed(Routes.verifyOtp);
-          }
-        },
-        builder: (context, state) {
-          return SafeArea(
+      body: BlocListener<SignUpBloc, SignUpState>(
+          listener: (context, state) {
+            if (state.status == SignUpStatus.success) {
+              _firstNameController.text = '';
+              _lastNameController.text = '';
+              _passwordController.text = '';
+              _birthDateController.text = '';
+              _emailController.text = '';
+              _phoneNumberController.text = '';
+              context.read<SignUpBloc>().add(SignUpStatusChange());
+            } else if (state.status == SignUpStatus.failure) {
+              showSnackBarMessage(
+                  context,
+                  _appLocalizations?.failedMessage ?? "",
+                  SnackBarMessageType.failure);
+            } else if (state.status == SignUpStatus.verify_otp) {
+              showSnackBarMessage(
+                  context, state.responseMessage, SnackBarMessageType.success);
+              context.goNamed(Routes.verifyOtp);
+            }
+          },
+          child: SafeArea(
             child: Center(
               child: SingleChildScrollView(
                 child: Form(
@@ -97,7 +98,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                         const SizedBox(
                           height: AppValues.padding,
                         ),
-                        _buildSubmitButton(state),
+                        _buildSubmitButton(),
                         const SizedBox(
                           height: AppValues.padding,
                         ),
@@ -113,9 +114,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 ),
               ),
             ),
-          );
-        },
-      ),
+          )),
     );
   }
 
@@ -169,88 +168,97 @@ class _SignUpScreenState extends State<SignUpScreen> {
   }
 
   Widget _buildFirsNameField() {
-    return Flexible(
-      child: ListTile(
-        contentPadding: EdgeInsets.zero,
-        title: Padding(
-          padding: const EdgeInsets.only(left: AppValues.margin_2),
-          child: Text(
-            _appLocalizations?.firstNameFormTitle ?? "",
+    return SignUpLoadingSelector((currentStatus) {
+      return Flexible(
+        child: ListTile(
+          contentPadding: EdgeInsets.zero,
+          title: Padding(
+            padding: const EdgeInsets.only(left: AppValues.margin_2),
+            child: Text(
+              _appLocalizations?.firstNameFormTitle ?? "",
+            ),
+          ),
+          subtitle: Padding(
+            padding: const EdgeInsets.only(top: AppValues.smallMargin),
+            child: AppTextField(
+              isEnabled: currentStatus == false,
+              onChanged: (value) {
+                context.read<SignUpBloc>().add(
+                      FirstNameChangeEvent(
+                        firstName: value ?? "",
+                      ),
+                    );
+              },
+              controller: _firstNameController,
+              validator: InputValidators.name,
+              labelText: _appLocalizations?.firstNameFormLabel ?? "",
+            ),
           ),
         ),
-        subtitle: Padding(
-          padding: const EdgeInsets.only(top: AppValues.smallMargin),
-          child: AppTextField(
-            onChanged: (value) {
-              context.read<SignUpBloc>().add(
-                    FirstNameChangeEvent(
-                      firstName: value ?? "",
-                    ),
-                  );
-            },
-            controller: _firstNameController,
-            validator: InputValidators.name,
-            labelText: _appLocalizations?.firstNameFormLabel ?? "",
-          ),
-        ),
-      ),
-    );
+      );
+    });
   }
 
   Widget _buildLastNameField() {
-    return Flexible(
-      child: ListTile(
+    return SignUpLoadingSelector((currentStatus) {
+      return Flexible(
+        child: ListTile(
+          contentPadding: EdgeInsets.zero,
+          title: Padding(
+            padding: const EdgeInsets.only(left: AppValues.margin_2),
+            child: Text(
+              _appLocalizations?.lastNameFormTitle ?? "",
+            ),
+          ),
+          subtitle: Padding(
+            padding: const EdgeInsets.only(top: AppValues.smallMargin),
+            child: AppTextField(
+              isEnabled: currentStatus == false,
+              onChanged: (value) {
+                context.read<SignUpBloc>().add(
+                      LastNameChangeEvent(
+                        lastName: value ?? "",
+                      ),
+                    );
+              },
+              controller: _lastNameController,
+              validator: InputValidators.name,
+              labelText: _appLocalizations?.lastNameFormLabel ?? "",
+            ),
+          ),
+        ),
+      );
+    });
+  }
+
+  Widget _buildEmailField() {
+    return SignUpLoadingSelector((currentStatus) {
+      return ListTile(
         contentPadding: EdgeInsets.zero,
         title: Padding(
           padding: const EdgeInsets.only(left: AppValues.margin_2),
           child: Text(
-            _appLocalizations?.lastNameFormTitle ?? "",
+            _appLocalizations?.fieldLabelTextEmail ?? "",
           ),
         ),
         subtitle: Padding(
           padding: const EdgeInsets.only(top: AppValues.smallMargin),
           child: AppTextField(
-            onChanged: (value) {
-              context.read<SignUpBloc>().add(
-                    LastNameChangeEvent(
-                      lastName: value ?? "",
-                    ),
-                  );
-            },
-            controller: _lastNameController,
-            validator: InputValidators.name,
-            labelText: _appLocalizations?.lastNameFormLabel ?? "",
-          ),
+              isEnabled: currentStatus == false,
+              onChanged: (value) {
+                context.read<SignUpBloc>().add(
+                      EmailChangeEvent(
+                        email: value ?? "",
+                      ),
+                    );
+              },
+              controller: _emailController,
+              validator: InputValidators.email,
+              labelText: _appLocalizations?.emailLabel ?? "",
+              prefix: const Icon(Icons.email_outlined)),
         ),
-      ),
-    );
-  }
-
-  Widget _buildEmailField() {
-    return ListTile(
-      contentPadding: EdgeInsets.zero,
-      title: Padding(
-        padding: const EdgeInsets.only(left: AppValues.margin_2),
-        child: Text(
-          _appLocalizations?.fieldLabelTextEmail ?? "",
-        ),
-      ),
-      subtitle: Padding(
-        padding: const EdgeInsets.only(top: AppValues.smallMargin),
-        child: AppTextField(
-            onChanged: (value) {
-              context.read<SignUpBloc>().add(
-                    EmailChangeEvent(
-                      email: value ?? "",
-                    ),
-                  );
-            },
-            controller: _emailController,
-            validator: InputValidators.email,
-            labelText: _appLocalizations?.emailLabel ?? "",
-            prefix: const Icon(Icons.email_outlined)),
-      ),
-    );
+      );
+    });
   }
 
   Image _buildAppHeader() {
@@ -263,75 +271,79 @@ class _SignUpScreenState extends State<SignUpScreen> {
   }
 
   Widget _buildPhoneNumberField() {
-    return ListTile(
-      contentPadding: EdgeInsets.zero,
-      title: Padding(
-        padding: const EdgeInsets.only(left: AppValues.margin_2),
-        child: Text(
-          _appLocalizations?.fieldLabelTextPhone ?? "",
+    return SignUpLoadingSelector((currentStatus) {
+      return ListTile(
+        contentPadding: EdgeInsets.zero,
+        title: Padding(
+          padding: const EdgeInsets.only(left: AppValues.margin_2),
+          child: Text(
+            _appLocalizations?.fieldLabelTextPhone ?? "",
+          ),
         ),
-      ),
-      subtitle: Padding(
-        padding: const EdgeInsets.only(top: AppValues.smallMargin),
-        child: AppTextField(
-          onChanged: (value) {
-            context.read<SignUpBloc>().add(
-                  PhoneNumberChangeEvent(
-                    phoneNumber: value ?? "",
-                  ),
-                );
-          },
-          controller: _phoneNumberController,
-          validator: InputValidators.phone,
-          labelText: _appLocalizations?.phoneLabel ?? "",
-          prefix: const Icon(Icons.phone_iphone_outlined),
+        subtitle: Padding(
+          padding: const EdgeInsets.only(top: AppValues.smallMargin),
+          child: AppTextField(
+            isEnabled: currentStatus == false,
+            onChanged: (value) {
+              context.read<SignUpBloc>().add(
+                    PhoneNumberChangeEvent(
+                      phoneNumber: value ?? "",
+                    ),
+                  );
+            },
+            controller: _phoneNumberController,
+            validator: InputValidators.phone,
+            labelText: _appLocalizations?.phoneLabel ?? "",
+            prefix: const Icon(Icons.phone_iphone_outlined),
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 
   Widget _buildPasswordTextField() {
-    return ListTile(
-      contentPadding: EdgeInsets.zero,
-      title: Padding(
-        padding: const EdgeInsets.only(left: AppValues.margin_2),
-        child: Text(
-          _appLocalizations?.fieldTitlePassword ?? "",
+    return SignUpLoadingSelector((currentStatus) {
+      return ListTile(
+        contentPadding: EdgeInsets.zero,
+        title: Padding(
+          padding: const EdgeInsets.only(left: AppValues.margin_2),
+          child: Text(
+            _appLocalizations?.fieldTitlePassword ?? "",
+          ),
         ),
-      ),
-      subtitle: Padding(
-        padding: const EdgeInsets.only(
-          top: AppValues.smallMargin,
+        subtitle: Padding(
+          padding: const EdgeInsets.only(
+            top: AppValues.smallMargin,
+          ),
+          child: AppTextField(
+              isEnabled: currentStatus == false,
+              prefix: const Icon(Icons.password_outlined),
+              controller: _passwordController,
+              labelText: _appLocalizations?.fieldLabelTextPassword ?? "",
+              onChanged: (value) {
+                context
+                    .read<SignUpBloc>()
+                    .add(PasswordChangeEvent(password: value.toString()));
+              },
+              validator: InputValidators.password,
+              obscureText: true),
         ),
-        child: AppTextField(
-            prefix: const Icon(Icons.password_outlined),
-            controller: _passwordController,
-            labelText: _appLocalizations?.fieldLabelTextPassword ?? "",
-            onChanged: (value) {
-              context
-                  .read<SignUpBloc>()
-                  .add(PasswordChangeEvent(password: value.toString()));
-            },
-            validator: InputValidators.password,
-            obscureText: true),
-      ),
-    );
+      );
+    });
   }
 
-  Widget _buildSubmitButton(SignUpState state) {
-    return state.status == SignUpStatus.loading
-        ? const Center(
-            child: CircularProgressIndicator(
-              color: AppColors.colorPrimary,
-            ),
-          )
-        : AppPrimaryButton(
-            onPressed: () => {
-              if (_formKey.currentState!.validate())
-                {context.read<SignUpBloc>().add(SignUpSubmitted())}
-            },
-            title: _appLocalizations?.signUp ?? "",
-          );
+  Widget _buildSubmitButton() {
+    return SignUpLoadingSelector((currentStatus) {
+      return currentStatus == false
+          ? AppPrimaryButton(
+              onPressed: () => {
+                if (_formKey.currentState!.validate())
+                  {context.read<SignUpBloc>().add(SignUpSubmitted())}
+              },
+              title: _appLocalizations?.signUp ?? "",
+            )
+          : CircularProgressIndicator();
+    });
   }
 
   Widget _buildFooter(BuildContext context) {
--- lib/features/sign_up/presentation/widgets/otp_verificaticion_widget.dart
+++ lib/features/sign_up/presentation/widgets/otp_verificaticion_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
+import 'package:qs_flutter/features/sign_up/presentation/bloc/sign_up_selector.dart';
 
 import '../../../../core/base/widgets/app_primary_button.dart';
 import '../../../../core/base/widgets/app_textfield.dart';
@@ -26,8 +27,8 @@ class _OTPVerificationViewState extends State<OTPVerificationView> {
   Widget build(BuildContext context) {
     _appLocalizations = AppLocalizations.of(context);
 
-    return BlocConsumer<SignUpBloc, SignUpState>(
-        listener: (context,state){
+    return BlocListener<SignUpBloc, SignUpState>(
+        listener: (context, state) {
           if (state.status == SignUpStatus.success) {
             showSnackBarMessage(
                 context, "Successfully Signed Up", SnackBarMessageType.success);
@@ -38,45 +39,51 @@ class _OTPVerificationViewState extends State<OTPVerificationView> {
                 context, "Something is wrong", SnackBarMessageType.failure);
           }
         },
-        builder: (context, state) {
-      return Scaffold(
-        appBar: AppBar(),
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  ListTile(
-                      contentPadding: EdgeInsets.zero,
-                      title: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: Text(_appLocalizations?.fieldLabelTextOTP ?? ""),
-                      ),
-                      subtitle: AppTextField(
-                          controller: _otpEditingController,
-                          validator: InputValidators.otp,
-                          labelText: _appLocalizations?.fieldTitleOTP ?? "",
-                          onChanged: (value) {
-                            context
-                                .read<SignUpBloc>()
-                                .add(OTPChangeEvent(otp: value.toString()));
-                          })),
-                  state.status == SignUpStatus.loading
-                      ? const CircularProgressIndicator()
-                      : AppPrimaryButton(
-                          onPressed: () {
-                            context.read<SignUpBloc>().add(VerifyOTPEvent());
-                          },
-                          title: _appLocalizations?.verifyOTP ?? "")
-                ],
+        child: Scaffold(
+          appBar: AppBar(),
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ListTile(
+                        contentPadding: EdgeInsets.zero,
+                        title: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child:
+                              Text(_appLocalizations?.fieldLabelTextOTP ?? ""),
+                        ),
+                        subtitle: SignUpLoadingSelector((currentStatus) {
+                          return AppTextField(
+                              isEnabled: currentStatus == false,
+                              controller: _otpEditingController,
+                              validator: InputValidators.otp,
+                              labelText: _appLocalizations?.fieldTitleOTP ?? "",
+                              onChanged: (value) {
+                                context
+                                    .read<SignUpBloc>()
+                                    .add(OTPChangeEvent(otp: value.toString()));
+                              });
+                        })),
+                    SignUpLoadingSelector((currentStatus) {
+                      return currentStatus == false
+                          ? AppPrimaryButton(
+                              onPressed: () {
+                                context
+                                    .read<SignUpBloc>()
+                                    .add(VerifyOTPEvent());
+                              },
+                              title: _appLocalizations?.verifyOTP ?? "")
+                          : const CircularProgressIndicator();
+                    })
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-      );
-    });
+        ));
   }
 }
